901f715bff2d7d19bd1f9049b2cb9fa4ced98c44
许大仙 <1900919313@qq.com>
2025-04-11 22:40:26 +0800

    feat: 初始化 Java 项目
    - 添加 Gradle 构建脚本和相关配置文件
    - 创建基本的项目结构和示例代码
    - 设置 Git 属性和忽略规则

---
+++ src/main/java/com/github/Application.java
@@ -0,0 +1,12 @@
+package com.github;
+
+/**
+ * @author 许大仙
+ * @version 1.0
+ * @since 2025-04-11 22:29
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/github/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("abc");
+    }
+
+}
